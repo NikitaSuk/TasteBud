789b8e585f0d2239feb3c03ba655ad62a479ada1
itsbora <126906765+itsbora@users.noreply.github.com>
2024-04-23 11:58:59 -0400

    Add files via upload

---
+++ TasteBudApp/TasteBudApp/TasteBudAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasteBudAppApp.swift
+//  TasteBudApp
+//
+//  Created by Bora Tekin on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TasteBudAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

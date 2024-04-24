4a2f5f00f7d0ea56488a58131a7a4c51153b65e2
Nikita Sukharevich <sukharevichn@gmail.com>
2024-04-23 22:04:55 -0400

    changed some files around

--- app/recommendation.py
+++ app/recommendation.py
@@ -1,1 +1,16 @@
+refined_options = {
+    'American': ['American', 'Bar', 'Burgers', 'BBQ', 'Sports Bar', 'Burger', 'New American', 'Cheesecake', 'Diner', 'Grill', 'Southern'],
+    'Italian': ['Italian', 'Mediterranean', 'Greek', 'Pizza', 'Pasta', 'Deli'],
+    'Mexican': ['Mexican', 'Fast Food', 'Sports Bar', 'Seafood', 'Asian', 'Salvadoran', 'Grill', 'Exotic', 'Fine Dining', 'Family-friendly'],
+    'Asian': ['Thai', 'Ramen', 'Seafood', 'Buffet', 'Vietnamese', 'Korean', 'Japanese', 'Chinese'],
+    'Seafood': ['Seafood', 'Steak', 'Sushi'],
+    'Fast Food': ['Fast Food', 'Chicken Sandwiches', 'Burgers', 'Sandwiches'],
+    'Sandwiches & Deli': ['Takeout', 'Cafe', 'Bakery', 'Sandwich'],
+    'Pizza': ['Pizza'],
+    'Steak & BBQ': ['Steak', 'Sushi', 'Burgers'],
+    'Other International': ['Eastern European', 'Pan-European', 'French', 'Jamaican', 'Dominican', 'Bangladeshi', 'Barbeque', 'African', 'Colombian', 'Wings', 'Indian', 'Exotic', 'Brunch', 'Sports Bar', 'Restaurant', 'Fine Dining'],
+    'Other': ['Bar', 'Chicken', 'New American', 'Exotic', 'Brunch', 'Family-friendly', 'Sports Bar', 'Restaurant', 'French', 'Grill']
+}
 
+def get_refined_options(selected_food_type):
+    return refined_options.get(selected_food_type, [])
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,18 @@
+from flask import render_template, request, jsonify
+from app import app
+from app.recommendation import get_refined_options
 
+@app.route('/')
+def index():
+    food_types = [
+        'American', 'Italian', 'Mexican', 'Asian', 'Seafood', 
+        'Fast Food', 'Sandwiches & Deli', 'Pizza', 'Steak & BBQ', 
+        'Other International', 'Other'
+    ]
+    return render_template('index.html', food_types=food_types)
+
+@app.route('/get_refined_options', methods=['POST'])
+def get_refined_options_route():
+    selected_food_type = request.json['selected_food_type']
+    options = get_refined_options(selected_food_type)
+    return jsonify(options)
